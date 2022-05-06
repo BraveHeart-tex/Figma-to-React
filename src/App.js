bbfc51ca93a3f81dec0f764b17cc0b576884b53e
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-05-06 17:55:00 +0300

    finished navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { Article, Brand, CTA, Feature, Navbar } from './components/index';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from './containers';
+import { Brand, CTA, Navbar } from './components';
+
+import './App.css';
+
 function App() {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './whatgpt3.css';
+
 function WhatGPT3() {
   return <div>WhatGPT3</div>;
 }
